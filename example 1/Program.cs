d669b227815195a63b2cc1f0559ddfb1d2152389
Mohammed Aloshaibi <mohammed.aloshaiby@gmail.com>
2024-01-23 17:02:42 +0300

    first commit

---
+++ example 1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.ComponentModel;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        //Dia.triangle();
+        //Dia.triang();
+        //Dia.squer();
+        Dia.tall();
+    }
+}
+
+class Dia
+{
+    public static void triangle()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int j = 1; j <= 10 - i; j++)
+                Console.Write(" ");
+            for (int j = 1; j <= i * 2 - 1; j++)
+            {
+                Console.Write("*");
+
+            }
+            Console.WriteLine();
+        }
+    }
+
+   
+
+    public static void squer()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int j = 1; j <= 10; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public static void triang()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int j = 1; j <= i; j++)
+
+                Console.Write("*");
+
+            Console.WriteLine();
+        }
+    }
+    public static void tall()
+    {
+        string[] words = { "mohammed", "khaled", "subhi" };
+        string longestWord = ""; 
+        int longer = 0; 
+        for (int i = 0; i < words.Length; i++) 
+        {
+            if (words[i].Length > longer) 
+            {
+                longer = words[i].Length; 
+                longestWord = words[i]; 
+            }
+        }
+
+        Console.WriteLine("The length of the longest word = " + longer);
+        Console.WriteLine("The longest word = " + longestWord);
+    }
+}
